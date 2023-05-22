d57951cdf1c9e22c61830f7f9cde1724b789d7d3
rodolphomoura <134257269+rodolphomoura@users.noreply.github.com>
2023-05-22 02:58:34 -0300

    Add files via upload

---
+++ PrimeiroPrograma/src/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package primeiroprograma;
+
+ class PrimeiroPrograma {
+
+     public static void main(String[] args) {
+         
+      
+        System.out.println("Ola Mundo");
+     
+    }
+    
+}
